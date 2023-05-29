e4f026e5876ab59266d703d4d8cb92968a6c37bc
Yan Gabriel <yan.leitao26@gmail.com>
2023-05-29 09:18:10 -0300

    Reestruturação das pastas do projeto

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,10 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core',
-    'item',
-    'dashboard',
-    'conversation'
+    'apps.core',
+    'apps.item',
+    'apps.dashboard',
+    'apps.conversation'
 ]
 
 MIDDLEWARE = [
@@ -62,8 +63,8 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
--- project/urls.py
+++ project/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('core.urls')),
-    path('items/', include('item.urls')),
-    path('dashboard/', include('dashboard.urls')),
-    path('inbox/', include('conversation.urls')),
+    path('', include('apps.core.urls')),
+    path('items/', include('apps.item.urls')),
+    path('dashboard/', include('apps.dashboard.urls')),
+    path('inbox/', include('apps.conversation.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
